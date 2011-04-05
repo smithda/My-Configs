c89d5ed2078fd6a7cec92dd1ebffa98096110ca3
Douglas Smith <kc8peg@yahoo.com>
2011-04-05 13:41:20 -0400

    Uploaded current config files.

---
+++ autostart.sh
@@ -0,0 +1,15 @@
+#.config/openbox/autostart.sh
+xcompmgr &
+tint2 &
+#terminator -m &
+#urxvt &
+tilda &
+wicd-gtk &
+volumeicon &
+xfce4-power-manager &
+#orage &
+
+#My wallpaper
+nitrogen --restore &
+
+xscreensaver -no-splash &
